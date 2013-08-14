c6a6bbab850bde8ba9efc1b79f37aac5853824fa
Davis Clark <jdc0589@gmail.com>
2013-08-13 20:48:43 -0500

    remove un-necessary test

--- tests/factory_lifecycle.js
+++ tests/factory_lifecycle.js
@@ -17,28 +17,6 @@ exports["singleton factory returns same reference"] = function(test) {
 	test.done();
 }
 
-exports["singleton factory (injected params) returns same reference"] = function(test) {
-	var fuel = new Fuel();
-
-	fuel
-		.value("$value", "this is the value")
-		.factory("testFactory", function($value) {
-			return {
-				output: $value
-			};
-		}, "singleton");
-
-	var result1 = fuel.get("testFactory");
-	var result2 = fuel.get("testFactory");
-
-	test.equal(result1, result2);
-	test.deepEqual(result1, result2);
-	test.deepEqual(result1, {output: "this is the value"});
-	
-	test.done();
-}
-
-
 exports["transient factory returns different references"] = function(test) {
 	var fuel = new Fuel();
 
